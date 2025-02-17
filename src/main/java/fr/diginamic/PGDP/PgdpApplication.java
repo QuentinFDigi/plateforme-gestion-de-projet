161cf972551d81bbb77785bee47d87a179c04328
Valentin JOUFFRAY <valentin.jouffray@gmail.com>
2025-02-17 10:56:52 +0100

    configure(mavenRepositories): configured pom.xml

---
+++ src/main/java/fr/diginamic/PGDP/PgdpApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.PGDP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/PGDP/PgdpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.PGDP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
